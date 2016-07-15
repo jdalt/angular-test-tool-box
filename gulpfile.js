bd9b197ff8566c5bd99665079d0c7fddfff6769d
Jacob Dalton <jacob.dalton@sportngin.com>
2016-07-15 13:13:36 -0500

    gulp: build on watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var ngAnnotate = require('gulp-ng-annotate')
+var watch = require('gulp-watch')
 
 
-gulp.task('build', function() {
-  return gulp.src(['./src/module.js','./src/!(-test).js'])
+gulp.task('build', build)
+gulp.task('watch', function() {
+  watch('./src/*-helper.js', function(file) {
+    console.log('File changed:', file.path)
+    build()
+  })
+})
+
+function build() {
+  return gulp.src(['./src/_module.js','./src/*-helper.js'])
     .pipe(concat('tool-box.js'))
     .pipe(ngAnnotate())
     .pipe(gulp.dest('./dist/'))
-})
+}
