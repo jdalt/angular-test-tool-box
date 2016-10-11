7b35cbb7b047bd3f218bbb0ef54f638131dc4be4
Jacob Dalton <jacob.dalton@sportngin.com>
2016-10-11 14:22:46 -0500

    group Many and One tests into suites

--- test/js-data-request-test.js
+++ test/js-data-request-test.js
@@ -80,66 +80,74 @@ describe('JsData Request', function() {
   })
 
   describe('resource definition expectations', function() {
-    it('should throw an exception when resource definition "junk" is not found', function() {
-      expect(function() { Req.expectOne('junk', 1) }).toThrow(new Error('No fabricator definition for junk'))
+    describe('"One" request', function() {
+
+      it('should throw an exception when resource definition "junk" is not found', function() {
+        expect(function() { Req.expectOne('junk', 1) }).toThrow(new Error('No fabricator definition for junk'))
+      })
+
+      it('should compile with expectation of "cat"', function() {
+        Req.expectOne('cat', 1)
+        dom.click('#catnip').flush()
+      })
+
+      it('should request cat and display cat name of default cat fabricator in #litter-box', function() {
+        Req.expectOne('cat', 1)
+        dom.click('#catnip').flush()
+        expect(dom.text('#litter-box')).toBe('Snarl')
+      })
+
+      it('should request cat and display cat name of default cat fabricator in #litter-box by getting id from response', function() {
+        Req.expectOne('cat')
+        dom.click('#catnip').flush()
+        expect(dom.text('#litter-box')).toBe('Snarl')
+      })
+
+      it('should request cat and display cat name in #litter-box', function() {
+        Req.expectOne('cat', 1, { name: 'Meowth' })
+        dom.click('#catnip').flush()
+        expect(dom.text('#litter-box')).toBe('Meowth')
+      })
+
     })
 
-    it('should throw an exception when response is not an array for expectMany', function() {
-      expect(function() {
-        Req.expectMany('cat', {}, { name: 'Not Array' })
-      }).toThrow(new Error('Response must be an array'))
-    })
+    describe('"Many" request', function() {
 
-    it('should compile with expectation of "cat"', function() {
-      Req.expectOne('cat', 1)
-      dom.click('#catnip').flush()
-    })
+      it('should throw an exception when response is not an array', function() {
+        expect(function() {
+          Req.expectMany('cat', {}, { name: 'Not Array' })
+        }).toThrow(new Error('Response must be an array'))
+      })
 
-    it('should request cat and display cat name of default cat fabricator in #litter-box', function() {
-      Req.expectOne('cat', 1)
-      dom.click('#catnip').flush()
-      expect(dom.text('#litter-box')).toBe('Snarl')
-    })
+      it('should request monkey and display monkey names <li>', function() {
+        Req.expectMany('monkey', { bunch: 10 }, [{ id: 1, name: 'Monkey 1' }, { id: 2, name: 'Monkey 2', bananas: 77 }])
+        dom.click('#monkey-button').flush()
 
-    it('should request cat and display cat name of default cat fabricator in #litter-box by getting id from response', function() {
-      Req.expectOne('cat')
-      dom.click('#catnip').flush()
-      expect(dom.text('#litter-box')).toBe('Snarl')
-    })
+        expect(dom.text('ul li')).toContain('Monkey 1')
+        expect(dom.text('ul li')).toContain('10')
+        expect(dom.text('ul li')).toContain('Monkey 2')
+        expect(dom.text('ul li')).toContain('77')
+      })
 
-    it('should request cat and display cat name in #litter-box', function() {
-      Req.expectOne('cat', 1, { name: 'Meowth' })
-      dom.click('#catnip').flush()
-      expect(dom.text('#litter-box')).toBe('Meowth')
-    })
+      it('should return empty array when no "res" arg are sent', function() {
+        Req.expectMany('monkey', { bunch: 10 })
+        dom.click('#monkey-button').flush()
+        expect(dom.count('ul li')).toBe(0)
+      })
 
-    it('should request monkey and display monkey names <li>', function() {
-      Req.expectMany('monkey', { bunch: 10 }, [{ id: 1, name: 'Monkey 1' }, { id: 2, name: 'Monkey 2', bananas: 77 }])
-      dom.click('#monkey-button').flush()
+      it('should extend passed object with fabricator object', function() {
+        Req.expectMany('monkey', { bunch: 10 }, [{}])
+        dom.click('#monkey-button').flush()
+        expect(dom.text('ul li', 0)).toContain('Mustapha')
+        expect(dom.text('ul li', 0)).toContain('10')
+      })
 
-      expect(dom.text('ul li')).toContain('Monkey 1')
-      expect(dom.text('ul li')).toContain('10')
-      expect(dom.text('ul li')).toContain('Monkey 2')
-      expect(dom.text('ul li')).toContain('77')
-    })
+      it('should return empty array when no "res" arg sent', function() {
+        Req.whenMany('monkey', { bunch: 10 })
+        dom.click('#monkey-button').flush()
+        expect(dom.count('ul li')).toBe(0)
+      })
 
-    it('should return empty array when no "res" arg are sent', function() {
-      Req.expectMany('monkey', { bunch: 10 })
-      dom.click('#monkey-button').flush()
-      expect(dom.count('ul li')).toBe(0)
-    })
-
-    it('should extend passed object with fabricator object', function() {
-      Req.expectMany('monkey', { bunch: 10 }, [{}])
-      dom.click('#monkey-button').flush()
-      expect(dom.text('ul li', 0)).toContain('Mustapha')
-      expect(dom.text('ul li', 0)).toContain('10')
-    })
-
-    it('should return empty array when no "res" arg sent', function() {
-      Req.whenMany('monkey', { bunch: 10 })
-      dom.click('#monkey-button').flush()
-      expect(dom.count('ul li')).toBe(0)
     })
 
     describe('nested resources', function() {
